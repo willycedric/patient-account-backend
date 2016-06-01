8a2709f34c34a28a2f2ceb232de5392f48164f7a
Willy DONGMO <w.dongmo@gmail.com>
2016-06-01 14:26:13 +0200

    chore

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ var config = {
   dev: 'development',
   test: 'testing',
   prod: 'production',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   // 10 days in minutes
   expireTime: 24 * 60 * 10,
   secrets: {
