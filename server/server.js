c95ef3bc95959d4427a8c1c2c2d3a8a66b0f3ca8
Willy DONGMO <w.dongmo@gmail.com>
2016-07-19 16:06:59 +0200

    chore step-2

--- server/server.js
+++ server/server.js
@@ -3,10 +3,7 @@ var app = express();
 var api = require('./api/api');
 var config = require('./config/config');
 var logger = require('./util/logger');
-//Socket io for live update on user creation, update and delete
-//var io = require('socket.io')(4001);
-var server = require('http').Server(app);
-var io =require('socket.io')(server);
+
 
 //var auth = require('./auth/routes');
 // db.url is different depending on NODE_ENV
@@ -27,7 +24,7 @@ app.use('/api', api);
 app.use(function(err, req, res, next) {
   // if error thrown from jwt validation check
   if (err.name === 'UnauthorizedError') {
-    res.status(401).send('Invalid token');
+    res.status(401).send('Invalid token'); 
     return;
   }
   logger.error(err);
